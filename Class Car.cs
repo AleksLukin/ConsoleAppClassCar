372e8dc6b3094eb126f4dbe7c48324ba81b3bb24
ALLUK <lam83@bk.ru>
2024-01-11 20:38:28 +0500

    Добавьте файлы проекта.

---
+++ Class Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClassCar
+{
+    internal class Class_Car
+    {
+        public void Movie(int s)
+        {
+                       
+            if (s==60)
+            {
+                Console.WriteLine("Машина едет со скоростью 60 километров в час "); 
+            }
+            else
+            {
+                Console.WriteLine("Машина стоит");
+            }
+            
+        } 
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClassCar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите скорость с которой едет машина: ");
+            string s = Console.ReadLine();
+
+            int.TryParse(s, out int result);
+
+            Class_Car car = new Class_Car();
+            car.Movie(result);
+
+            Console.ReadLine();
+        }
+    }
+}
